db1911c3cba51949d2dccbd6872a454052a4167b
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-05-26 17:57:07 -0500

    Update Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,22 @@
+//Time Complexity : O(n)
+//Space Complexity : O(n)
+// Solved on leetcode - yes
+// Did you face any issues - no
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        if(nums.length == 0 ) return null;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i< nums.length;i++){
+            int difference = target - nums[i];
+
+            if(map.containsKey(difference)){
+                return new int []{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }   
+        return null;
+    }
+}
